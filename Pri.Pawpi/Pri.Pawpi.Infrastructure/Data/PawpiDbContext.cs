e47e3df295842640c4ad2fe8a28a0181ee80b60b
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-01 19:45:45 +0200

    Add dbcontext

---
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Data/PawpiDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.Pawpi.Core.Entities;
+
+namespace Pri.Pawpi.Infrastructure.Data
+{
+    public class PawpiDbContext :DbContext
+    {
+        public DbSet<Practice> Practices { get; set; }
+        public DbSet<Veterinarian> Veterinarians { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Consultation> Consultations { get; set; } 
+        public DbSet<Medication> Medication { get; set; }
+
+        public PawpiDbContext(DbContextOptions<PawpiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Seeder.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Pri.Pawpi/Pri.Pawpi.Web/Program.cs
+++ Pri.Pawpi/Pri.Pawpi.Web/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Pri.Pawpi.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<PawpiDbContext>(
+    options => options
+    .UseSqlServer(builder.Configuration.GetConnectionString("PawpiDB"))
+    );
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
