d27a4af07a1ca164b053fcc6015804c18fd78295
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-01 18:54:54 +0200

    change contact entity to an interface

--- Pri.Pawpi/Pri.Pawpi.Core/Entities/BaseEntity.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Entities/BaseEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pri.Pawpi.Core.Entities
+﻿namespace Pri.Pawpi.Core.Entities
 {
     public abstract class BaseEntity
     {
