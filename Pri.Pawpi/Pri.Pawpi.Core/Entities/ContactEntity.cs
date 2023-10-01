394dbd3d65513e764ad6a6bcd80850e061ee0b4a
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-01 18:48:07 +0200

    Add contact entity

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Entities/ContactEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Pawpi.Core.Entities
+{
+    public abstract class ContactEntity
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Postal { get; set; }
+    }
+}
