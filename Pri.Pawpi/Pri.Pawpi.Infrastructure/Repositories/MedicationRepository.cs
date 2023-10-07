9d757ed66a61270d4461d778580e8de05c643611
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-07 19:20:58 +0200

    Add medication repository

---
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Repositories/MedicationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Interfaces.Repositories;
+using Pri.Pawpi.Infrastructure.Data;
+
+namespace Pri.Pawpi.Infrastructure.Repositories
+{
+    public class MedicationRepository : BaseRepository<Medication>, IMedicationRepository
+    {
+        public MedicationRepository(PawpiDbContext applicationDbContext, ILogger<BaseRepository<Medication>> logger) : base(PawpiDbContext, logger)
+        {
+        }
+    }
+}
