224c00b73e9a5b296086ef1f685089831abde6c7
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-07 13:39:54 +0200

    Add fluent api

--- Pri.Pawpi/Pri.Pawpi.Infrastructure/Data/PawpiDbContext.cs
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Data/PawpiDbContext.cs
@@ -18,6 +18,183 @@ namespace Pri.Pawpi.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region Practice Fluent API
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.Name)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.Address)
+                        .IsRequired(true)
+                        .HasMaxLength(100);
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.City)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.Email)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.Phone)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Practice>()
+                        .Property(p => p.Postal)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            #endregion
+
+            #region Veterinarian Fluent API
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.FirstName)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.LastName)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.Email)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.Phone)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.Address)
+                        .IsRequired(true)
+                        .HasMaxLength(100);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.City)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.Postal)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Veterinarian>()
+                        .Property(v => v.Birth)
+                        .IsRequired(true);
+            #endregion
+
+            #region Customer Fluent API
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.FirstName)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.LastName)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.Email)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.Phone)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.Address)
+                        .IsRequired(true)
+                        .HasMaxLength(100);
+            modelBuilder.Entity<Customer>()
+                     .Property(c => c.City)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.Postal)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Customer>()
+                        .Property(c => c.Birth)
+                        .IsRequired(true);
+            #endregion
+
+            #region Pet Fluent API
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.Name)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.CallName)
+                        .IsRequired(false)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.Breed)
+                        .IsRequired(false)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.Color)
+                        .IsRequired(false)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.AnimalType)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Pet>()
+                        .Property(p => p.Weight)
+                        .IsRequired(false);
+            #endregion
+
+            #region Consultation Fluent API
+            modelBuilder.Entity<Consultation>()
+                        .Property(c => c.Title)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Consultation>()
+                        .Property(c => c.Diagnosis)
+                        .IsRequired(true)
+                        .HasMaxLength(250);
+            modelBuilder.Entity<Consultation>()
+                        .Property(c => c.Treatment)
+                        .IsRequired(false)
+                        .HasMaxLength(250);
+            modelBuilder.Entity<Consultation>()
+                        .Property(c => c.Notes)
+                        .IsRequired(false)
+                        .HasMaxLength(250);
+            modelBuilder.Entity<Consultation>()
+                        .Property(c => c.DateOfConsultation)
+                        .IsRequired(true);
+            #endregion
+
+            #region Medication Fluent API
+            modelBuilder.Entity<Medication>()
+                        .Property(m => m.Name)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Medication>()
+                        .Property(m => m.Dosage)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Medication>()
+                        .Property(m => m.Frequency)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Medication>()
+                        .Property(m => m.Notes)
+                        .IsRequired(false)
+                        .HasMaxLength(250);
+            modelBuilder.Entity<Medication>()
+                        .Property(m => m.SideEffects)
+                        .IsRequired(false)
+                        .HasMaxLength(250);
+            #endregion
+
+            #region Specialty Fluent API
+            modelBuilder.Entity<Specialty>()
+                        .Property(s => s.Name)
+                        .IsRequired(true)
+                        .HasMaxLength(50);
+            modelBuilder.Entity<Specialty>()
+                        .Property(s => s.Description)
+                        .IsRequired(false)
+                        .HasMaxLength(250);
+            #endregion
+
             //Seeder.Seed(modelBuilder);
             base.OnModelCreating(modelBuilder);
         }
