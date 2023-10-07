67ee1b853d8035f9bc5cf37158bd57ad828076d9
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-07 19:12:19 +0200

    Add base repository

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pri.Pawpi.Core.Entities;
+
+namespace Pri.Pawpi.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<bool> CreateAsync(T toCreate);
+        Task<bool> UpdateAsync(T toUpdate);
+        Task<bool> DeleteAsync(T toDelete);
+        IQueryable<T> GetAll();
+    }
+}
