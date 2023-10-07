41894f84bcaf11d00a78923e0f7f4fc76e15d891
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-07 19:39:42 +0200

    Add consultation repository

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Repositories/IConsultationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Pri.Pawpi.Core.Entities;
+
+namespace Pri.Pawpi.Core.Interfaces.Repositories
+{
+    public interface IConsultationRepository : IBaseRepository<Consultation>
+    {
+        public Task<IEnumerable<Consultation>> SearchByPetIdAsync(int petId);
+        public Task<IEnumerable<Consultation>> SearchByVeterinarianIdAsync(int vetId);
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Repositories/ConsultationRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Interfaces.Repositories;
+using Pri.Pawpi.Infrastructure.Data;
+
+namespace Pri.Pawpi.Infrastructure.Repositories
+{
+    public class ConsultationRepository : BaseRepository<Consultation>, IConsultationRepository
+    {
+        public ConsultationRepository(PawpiDbContext PawPiDb, ILogger<BaseRepository<Consultation>> logger) : base(PawPiDb, logger)
+        {
+
+        }
+
+        public override async Task<IEnumerable<Consultation>> SearchByNameAsync(string name)
+        {
+            var consultations = GetAll();
+            return await consultations.Where(s => s.Title.ToUpper() == name.ToUpper()).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Consultation>> SearchByPetIdAsync(int petId)
+        {
+            var consultations = GetAll();
+            return await consultations.Where(s => s.PetId == petId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Consultation>> SearchByVeterinarianIdAsync(int vetId)
+        {
+            var consultations = GetAll();
+            return await consultations.Where(s => s.VeterinarianId == vetId).ToListAsync();
+        }
+    }
+}
