609322985b8f840404621bb64c7bc8cc6f6d7280
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-11 12:16:37 +0200

    Add specialty repository

---
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Repositories/SpecialtyRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Interfaces.Repositories;
+using Pri.Pawpi.Infrastructure.Data;
+
+namespace Pri.Pawpi.Infrastructure.Repositories
+{
+    public class SpecialtyRepository : BaseRepository<Specialty>, ISpecialtyRepository
+    {
+        public SpecialtyRepository(PawpiDbContext PawPiDb, ILogger<BaseRepository<Specialty>> logger) : base(PawPiDb, logger)
+        {
+
+        }
+
+        public override async Task<IEnumerable<Specialty>> SearchByNameAsync(string name)
+        {
+            var specialties = GetAll();
+            return await specialties.Where(s => s.Name.ToUpper().Contains(name.ToUpper())).ToListAsync();
+        }
+    }
+}
