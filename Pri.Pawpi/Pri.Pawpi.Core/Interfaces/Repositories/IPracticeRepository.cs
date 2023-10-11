6716ea15fcd19c580f0e8f9f5859964219a14eba
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-11 12:09:58 +0200

    Add practice repo

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Repositories/IPracticeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Pri.Pawpi.Core.Entities;
+
+namespace Pri.Pawpi.Core.Interfaces.Repositories
+{
+    public interface IPracticeRepository : IBaseRepository<Practice>
+    {
+        public Task<DateTime?> GetOpenTime(int practiceId);
+        public Task<DateTime?> GetClosureTime(int practiceId);
+        public Task<IEnumerable<Practice>> SearchByCity(string city);
+    }
+}
