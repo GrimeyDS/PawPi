41c8a39a76792ab9017a2a4aafddacb8152cfe32
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-16 20:33:15 +0200

    Add extension methods for model and pet

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Extensions/ModelExtensionMethods.cs
@@ -0,0 +1,51 @@
+﻿using Pri.Pawpi.Core.Services.Models;
+
+namespace Pri.Pawpi.Core.Extensions
+{
+    public static class ModelExtensionMethods
+    {
+        public static ResultModel<T> ToResultModel<T>(this IEnumerable<T> items)
+        {
+            var result = new ResultModel<T>
+            {
+                Items = items,
+                IsSuccess = true
+            };
+
+            return result;
+        }
+
+        public static ResultModel<T> ToErrorModel<T>(this IEnumerable<T> items, string errorMessage)
+        {
+            var result = new ResultModel<T>
+            {
+                Errors = new List<string> { errorMessage },
+                IsSuccess = false
+            };
+
+            return result;
+        }
+
+        public static ResultModel<T> ToResultModel<T>(this T item)
+        {
+            var result = new ResultModel<T>
+            {
+                Item = item,
+                IsSuccess = true
+            };
+
+            return result;
+        }
+
+        public static ResultModel<T> ToErrorModel<T>(this T item, string errorMessage)
+        {
+            var result = new ResultModel<T>
+            {
+                Errors = new List<string> { errorMessage },
+                IsSuccess = false
+            };
+
+            return result;
+        }
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Core/Extensions/PetExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Pri.Pawpi.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Pawpi.Core.Extensions
+{
+    public static class PetExtensions
+    {
+        public static bool CheckIfPetsExist(this IQueryable<Pet> pets, IEnumerable<int> petIdsToCheck)
+        {
+            if (pets.Where(p => petIdsToCheck.Contains(p.Id)).Count() != petIdsToCheck.Count())
+                return false;
+            return true;
+        }
+    }
+}
