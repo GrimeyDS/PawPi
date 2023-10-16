75fbf9a15b68aad1f78cd8f21d074fbf9574369b
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-16 20:34:41 +0200

    Add models

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Services/Models/Medication/MedicationAddModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Pri.Pawpi.Core.Services.Models.Medication
+{
+    public class MedicationAddModel
+    {
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public string SideEffects { get; set; }
+        public string Dosage { get; set; }
+        public string Frequency { get; set; }
+        public IEnumerable<int> PetIds { get; set; }
+    }
+}
