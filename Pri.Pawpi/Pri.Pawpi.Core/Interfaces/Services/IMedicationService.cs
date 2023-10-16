092a6b5c8a2f44d350993097631a78c7494f4a29
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-16 20:34:26 +0200

    Add interfaces for medication and pet

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Services/IMedicationService.cs
@@ -0,0 +1,15 @@
+﻿using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Services.Models;
+using Pri.Pawpi.Core.Services.Models.Medication;
+
+namespace Pri.Pawpi.Core.Interfaces.Services
+{
+    public interface IMedicationService
+    {
+        Task<ResultModel<Medication>> AddAsync(MedicationAddModel addModel);
+        Task<ResultModel<Medication>> UpdateAsync(MedicationUpdateModel updateModel);
+        Task<ResultModel<Medication>> DeleteAsync(int id);
+        Task<ResultModel<Medication>> GetAllAsync();
+        Task<ResultModel<Medication>> GetByIdAsync(int id);
+    }
+}
--- Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Services/IPetService.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Interfaces/Services/IPetService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pri.Pawpi.Core.Interfaces.Services
+﻿namespace Pri.Pawpi.Core.Interfaces.Services
 {
     public interface IPetService
     {
