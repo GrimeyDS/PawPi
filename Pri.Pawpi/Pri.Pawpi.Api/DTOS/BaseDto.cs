fadefbae5c38038ce2e5e4036f382807caf6e8bd
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-16 18:01:30 +0200

    Add base and medication dtos

---
+++ Pri.Pawpi/Pri.Pawpi.Api/DTOS/BaseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Pri.Pawpi.Api.DTOS
+{
+    public abstract class BaseDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Api/DTOS/MedicationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Pri.Pawpi.Api.DTOS
+{
+    public class MedicationDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SideEffects { get; set; }
+    }
+}
