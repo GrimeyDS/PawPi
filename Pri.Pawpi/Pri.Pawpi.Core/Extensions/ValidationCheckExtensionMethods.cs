62ca6c67cbbcebce45f78892c06d0b3d797b3473
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-17 19:21:14 +0200

    Rename and remove code duplication

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Extensions/ValidationCheckExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Pri.Pawpi.Core.Entities;
+
+
+namespace Pri.Pawpi.Core.Extensions
+{
+    public static class ValidationCheckExtensionsMethods
+    {
+        public static bool CheckIfIdsExist(this IQueryable<BaseEntity> list, IEnumerable<int> idsToCheck)
+        {
+            if (list.Where(p => idsToCheck.Contains(p.Id)).Count() != idsToCheck.Count())
+                return false;
+            return true;
+        }
+    }
+}
