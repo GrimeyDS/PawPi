4fd362a1f2dafc4a4cb635f9b7b42446cbc4236b
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-18 22:45:17 +0200

    Add customer controller

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.Pawpi.Api.Extensions;
+using Pri.Pawpi.Core.Interfaces.Services;
+using Pri.Pawpi.Core.Services;
+
+namespace Pri.Pawpi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        protected readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var customers = await _customerService.GetAllAsync();
+
+            var customerResponseDto = customers.Items.MapCustomersDto();
+
+            return Ok(customerResponseDto);
+        }
+    }
+}
