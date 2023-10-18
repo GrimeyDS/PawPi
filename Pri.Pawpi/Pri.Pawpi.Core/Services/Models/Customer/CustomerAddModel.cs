47a97592a2b54b789068c73d253b65b8f21200d3
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-18 22:46:14 +0200

    Add customer models

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Services/Models/Customer/CustomerAddModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Pawpi.Core.Services.Models.Customer
+{
+    public class CustomerAddModel
+    {
+    }
+}
