6973d55aa2f41a9ce4d71d1422c1da804eb1c701
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-18 18:24:03 +0200

    Add vet dto to specialty

--- Pri.Pawpi/Pri.Pawpi.Api/Dtos/Specialty/SpecialtyResponseDto.cs
+++ Pri.Pawpi/Pri.Pawpi.Api/Dtos/Specialty/SpecialtyResponseDto.cs
@@ -1,8 +1,11 @@
-﻿namespace Pri.Pawpi.Api.Dtos.Specialty
+﻿using Pri.Pawpi.Api.Dtos.Veterinarian;
+
+namespace Pri.Pawpi.Api.Dtos.Specialty
 {
     public class SpecialtyResponseDto : BaseDto
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public IEnumerable<VeterinarianResponseDto> Veterinarians { get; set; }
     }
 }
