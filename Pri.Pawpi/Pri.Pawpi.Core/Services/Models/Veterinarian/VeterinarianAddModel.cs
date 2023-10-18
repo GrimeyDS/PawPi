12124c63d0b406a0216cf61a678d916352f6f71e
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-18 18:25:07 +0200

    Add models for veterinarian

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Services/Models/Veterinarian/VeterinarianAddModel.cs
@@ -0,0 +1,10 @@
+﻿using Pri.Pawpi.Core.Entities;
+
+namespace Pri.Pawpi.Core.Services.Models.Veterinarian
+{
+    public class VeterinarianAddModel : Person
+    {
+        public List<int> SpecialtyIds { get; set; }
+        public List<int> PracticeIds { get; set; }
+    }
+}
