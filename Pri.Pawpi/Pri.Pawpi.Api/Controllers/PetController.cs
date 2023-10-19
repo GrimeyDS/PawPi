40a142eaad24c72401cd91eb7e324301608349ca
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 09:20:10 +0200

    Add pet controller

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Controllers/PetController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.Pawpi.Api.Extensions;
+using Pri.Pawpi.Core.Interfaces.Services;
+
+namespace Pri.Pawpi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : Controller
+    {
+        protected readonly IPetService _petService;
+
+        public PetController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var pets = await _petService.GetAllAsync();
+
+            var petResponseDto = pets.Items.MapPetsDto();
+
+            return Ok(petResponseDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var pet = await _petService.GetByIdAsync(id);
+
+            if (!pet.IsSuccess)
+                return BadRequest(pet.Errors);
+
+            var customerResponseDto = pet.Item.MapPetDto();
+
+            return Ok(customerResponseDto);
+        }
+    }
+}
