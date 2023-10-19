58719d4b652b91b9bc1ba2479749b14b640178fc
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 22:23:30 +0200

    Add id check

--- Pri.Pawpi/Pri.Pawpi.Core/Extensions/ValidationCheckExtensionMethods.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Extensions/ValidationCheckExtensionMethods.cs
@@ -1,5 +1,5 @@
 ﻿using Pri.Pawpi.Core.Entities;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Pri.Pawpi.Core.Extensions
 {
@@ -8,6 +8,14 @@ namespace Pri.Pawpi.Core.Extensions
         public static bool CheckIfIdsExist(this IQueryable<BaseEntity> list, IEnumerable<int> idsToCheck)
         {
             if (list.Where(p => idsToCheck.Contains(p.Id)).Count() != idsToCheck.Count())
+                return false;
+            return true;
+        }
+
+        public static bool CheckIdsInput(this IQueryable<BaseEntity> list, IEnumerable<int> idsToCheck)
+        {
+            var listCount = list.Where(l => idsToCheck.Contains(l.Id)).Count();
+            if (idsToCheck.Count() == 0 && listCount == 0)
                 return false;
             return true;
         }
