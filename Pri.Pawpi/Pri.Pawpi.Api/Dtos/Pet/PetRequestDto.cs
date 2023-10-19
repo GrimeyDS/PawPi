7a3b28e42cca11b915ecf66951964a979396fed4
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 09:20:30 +0200

    Add pet dtos

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Dtos/Pet/PetRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Pawpi.Api.Dtos.Pet
+{
+    public class PetRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        public string? CallName { get; set; }
+        public string? Breed { get; set; }
+        public string? Color { get; set; }
+
+        [Required(ErrorMessage = "Animal type is required")]
+        public string AnimalType { get; set; }
+        public double? Weight { get; set; }
+        //public List<ConsultationResponseDto> Consultations { get; set; }
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Api/Dtos/Pet/PetResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Pri.Pawpi.Api.Dtos.Pet
+{
+    public class PetResponseDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string CallName { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string AnimalType { get; set; }
+        public double Weight { get; set; }
+        //public List<ConsultationResponseDto> Consultations { get; set; }
+    }
+}
