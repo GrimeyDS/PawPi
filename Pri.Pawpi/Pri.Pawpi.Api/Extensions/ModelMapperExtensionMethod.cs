867527198e44638ecff0b68b1077bdfb2005f498
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 22:23:24 +0200

    Add model mapper extension for specialty

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Extensions/ModelMapperExtensionMethod.cs
@@ -0,0 +1,31 @@
+﻿using Pri.Pawpi.Api.Dtos.Specialty.Request;
+using Pri.Pawpi.Core.Services.Models.Specialty;
+
+namespace Pri.Pawpi.Api.Extensions
+{
+    public static class ModelMapperExtensionMethod
+    {
+        #region specialty model mapper
+        public static SpecialtyAddModel MapModel(this SpecialtyCreateDto specialtyCreateDto)
+        {
+            return new SpecialtyAddModel
+            {
+                Name = specialtyCreateDto.Name,
+                Description = specialtyCreateDto.Description,
+                VeterinarianIds = specialtyCreateDto.VeterinarianIds
+            };
+        }
+
+        public static SpecialtyUpdateModel MapModel(this SpecialtyUpdateDto specialtyUpdateDto)
+        {
+            return new SpecialtyUpdateModel
+            {
+                Id = specialtyUpdateDto.Id,
+                Name = specialtyUpdateDto.Name,
+                Description = specialtyUpdateDto.Description,
+                VeterinarianIds = specialtyUpdateDto.VeterinarianIds
+            };
+        }
+        #endregion
+    }
+}
