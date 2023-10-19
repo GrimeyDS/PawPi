17e1f7a2493592867f43b1fa2124258c93d17ce0
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 15:45:02 +0200

    Add consultation dtos

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Dtos/Consultation/ConsultationRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Pawpi.Api.Dtos.Consultation
+{
+    public class ConsultationRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Diagnosis is required")]
+        public string Diagnosis { get; set; }
+        public string? Treatment { get; set; }
+        public string? Notes { get; set; }
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime DateOfConsultation { get; set; }
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Api/Dtos/Consultation/ConsultationResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Pri.Pawpi.Api.Dtos.Consultation
+{
+    public class ConsultationResponseDto
+    {
+        public string Title { get; set; }
+        public string Diagnosis { get; set; }
+        public string? Treatment { get; set; }
+        public string? Notes { get; set; }
+        public DateTime DateOfConsultation { get; set; }
+    }
+}
