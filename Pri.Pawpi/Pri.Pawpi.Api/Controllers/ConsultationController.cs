4b7777136e2a371951502f28b086be0f2d6bb031
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-19 15:52:01 +0200

    Add controllers for consultation and practice

---
+++ Pri.Pawpi/Pri.Pawpi.Api/Controllers/ConsultationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Interfaces.Services;
+
+namespace Pri.Pawpi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultationController : BaseController<Consultation>
+    {
+        private readonly IConsultationService _consultationService;
+
+        public ConsultationController(IConsultationService consultationService) : base(consultationService)
+        {
+            _consultationService = consultationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var consultations = await _consultationService.GetAllAsync();
+
+            var consultationResponseDto = consultations.Items.MapConsultationsDto();
+
+            return Ok(consultationResponseDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var consultation = await _consultationService.GetByIdAsync(id);
+
+            if (!consultation.IsSuccess)
+                return BadRequest(consultation.Errors);
+
+            var consultationResponseDto = consultation.Item.MapConsultationDto();
+
+            return Ok(consultationResponseDto);
+        }
+    }
+}
---
+++ Pri.Pawpi/Pri.Pawpi.Api/Controllers/PracticeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.Pawpi.Core.Entities;
+using Pri.Pawpi.Core.Interfaces.Services;
+
+namespace Pri.Pawpi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PracticeController : BaseController<Practice>
+    {
+        private readonly IPracticeService _practiceService;
+
+        public PracticeController(IPracticeService practiceService) : base(practiceService)
+        {
+            _practiceService = practiceService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var practices = await _practiceService.GetAllAsync();
+
+            var practicesResponseDto = practices.Items.MapPracticesDto();
+
+            return Ok(practicesResponseDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var practice = await _practiceService.GetByIdAsync(id);
+
+            if (!practice.IsSuccess)
+                return BadRequest(practice.Errors);
+
+            var practiceResponseDto = practice.Item.MapPracticeDto();
+
+            return Ok(practiceResponseDto);
+        }
+    }
+}
