2f4047258e4e0a53dbd61656b99b26f0e7d36cdb
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-26 23:08:15 +0200

    Remove mapping items

--- Pri.Pawpi/Pri.Pawpi.Core/Extensions/ModelExtensionMethods.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Extensions/ModelExtensionMethods.cs
@@ -56,8 +56,6 @@ namespace Pri.Pawpi.Core.Extensions
 
         public static void MapEntity(this Person person, PersonModel personModel)
         {
-            person.FirstName = personModel.FirstName;
-            person.LastName = personModel.LastName;
             person.Birth = personModel.Birth;
             person.Address = personModel.Address;
             person.City = personModel.City;
