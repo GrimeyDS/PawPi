3d5f5038e3eb90fa83cc9df8a4500035224898cb
SvenDS91 <sven.de.schagt@student.howest.be>
2023-10-26 23:06:29 +0200

    Add migration for list to icollection change in entities

---
+++ Pri.Pawpi/Pri.Pawpi.Infrastructure/Migrations/20231026203935_UpdateListProperties.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pri.Pawpi.Infrastructure.Migrations
+{
+    public partial class UpdateListProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 26, 22, 39, 34, 903, DateTimeKind.Local).AddTicks(6771));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 26, 22, 39, 34, 903, DateTimeKind.Local).AddTicks(6812));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 26, 22, 39, 34, 903, DateTimeKind.Local).AddTicks(6817));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 26, 22, 39, 34, 903, DateTimeKind.Local).AddTicks(6820));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 26, 22, 39, 34, 903, DateTimeKind.Local).AddTicks(6823));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 24, 17, 33, 37, 924, DateTimeKind.Local).AddTicks(4676));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 24, 17, 33, 37, 924, DateTimeKind.Local).AddTicks(4772));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 24, 17, 33, 37, 924, DateTimeKind.Local).AddTicks(4776));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 24, 17, 33, 37, 924, DateTimeKind.Local).AddTicks(4784));
+
+            migrationBuilder.UpdateData(
+                table: "Consultations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateOfConsultation",
+                value: new DateTime(2023, 10, 24, 17, 33, 37, 924, DateTimeKind.Local).AddTicks(4787));
+        }
+    }
+}
