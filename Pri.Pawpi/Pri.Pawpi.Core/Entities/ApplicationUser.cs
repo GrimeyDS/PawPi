d7fae9b6d4fbd5e27e1005766bb4e71b5d0e26df
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-10 20:42:57 +0100

    Add applicationuser entity

---
+++ Pri.Pawpi/Pri.Pawpi.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Pri.Pawpi.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public Veterinarian Veterinarian { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
