5e9c9d41c6910cc6782d4b63f3eb97be6dfabb8d
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-10 20:40:56 +0100

    Remove unused usings

--- Pri.Pawpi/Pri.Pawpi.Core/Entities/Person.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Entities/Person.cs
@@ -1,9 +1,4 @@
 ﻿using Pri.Pawpi.Core.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pri.Pawpi.Core.Entities
 {
