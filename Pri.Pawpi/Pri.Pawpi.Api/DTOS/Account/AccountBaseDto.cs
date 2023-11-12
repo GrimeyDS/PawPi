7fecaeac10861bcd7f8cf166abf7e547da57fc42
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-12 14:38:52 +0100

    Add dtos for login

---
+++ Pri.Pawpi/Pri.Pawpi.Api/DTOS/Account/AccountBaseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Pawpi.Api.DTOS.Account
+{
+    public class AccountBaseDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
