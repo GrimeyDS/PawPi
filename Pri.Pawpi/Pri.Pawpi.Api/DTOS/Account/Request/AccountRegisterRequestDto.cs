a7cd3b21ce302589b9acaf999f5bfbe6d07b8be3
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-16 20:06:37 +0100

    Add dto for register

---
+++ Pri.Pawpi/Pri.Pawpi.Api/DTOS/Account/Request/AccountRegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Pawpi.Api.DTOS.Account.Request
+{
+    public class AccountRegisterRequestDto : AccountBaseDto
+    {
+        [Required]
+        [Compare("Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
