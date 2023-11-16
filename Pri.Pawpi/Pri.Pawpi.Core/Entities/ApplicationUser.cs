76e23a8995ec8b91e5ada7afdfe08ab0b428b811
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-16 20:03:20 +0100

    Add user nav properties for practice

--- Pri.Pawpi/Pri.Pawpi.Core/Entities/ApplicationUser.cs
+++ Pri.Pawpi/Pri.Pawpi.Core/Entities/ApplicationUser.cs
@@ -9,5 +9,7 @@ namespace Pri.Pawpi.Core.Entities
         public Veterinarian Veterinarian { get; set; }
         public int? CustomerId { get; set; }
         public Customer Customer { get; set; }
+        public int? PracticeId { get; set; }
+        public Practice Practice { get; set; }
     }
 }
