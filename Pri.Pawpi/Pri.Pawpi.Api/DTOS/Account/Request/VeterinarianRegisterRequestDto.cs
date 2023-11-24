703f8c70b5eea93f8fc11e694352cbb2d5cb913c
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-24 14:28:00 +0100

    Change register dto to 2 dtos for customer and veterinarian

---
+++ Pri.Pawpi/Pri.Pawpi.Api/DTOS/Account/Request/VeterinarianRegisterRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using Pri.Pawpi.Api.Dtos.Veterinarian.Request;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pri.Pawpi.Api.DTOS.Account.Request
+{
+    public class VeterinarianRegisterRequestDto : VeterinarianCreateDto
+    {
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
