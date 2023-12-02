826673a9b1e77ff45e3220ecb5e314c8d4f67717
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-02 15:49:26 +0100

    Add show practices

--- Pri.Pawpi/Pri.Pawpi.Vue/wwwroot/js/app.js
+++ Pri.Pawpi/Pri.Pawpi.Vue/wwwroot/js/app.js
@@ -1,15 +1,87 @@
-﻿let practices = new Vue({
-    el: '#practices',
+﻿let app = new Vue({
+    el: '#app',
         data: {
-            pageTitle: 'Practices!',
+            pageTitle: '',
+            homeVisible: true,
             practicesVisible: false,
+            veterinariansVisible: false,
+            customersVisible: false,
+            petsVisible: false,
+            consultationsVisible: false,
+            specialtiesVisible: false,
+            medicationsVisible: false,
+            isLoading: false,
+
+            baseUrl: 'https://localhost:7031/api',
+
+            practices: null,
     },
 
+        
 
+    methods: {
+        setNav: function (navItem) {
+            this.homeVisible = false;
+            this.practicesVisible = false;
+            this.veterinariansVisible = false;
+            this.customersVisible = false;
+            this.petsVisible = false;
+            this.consultationsVisible = false;
+            this.specialtiesVisible = false;
+            this.medicationsVisible = false;
 
-        methods: {
-            getPractices: function () {
-                this.practicesVisible = !this.practicesVisible;
-                }
+            switch (navItem) {
+                case 'Practices':
+                    this.practicesVisible = true;
+                    break;
+                case 'Veterinarians':
+                    this.veterinariansVisible = true;
+                    break;
+                case 'Customers':
+                    this.customersVisible = true;
+                    break;
+                case 'Pets':
+                    this.petsVisible = true;
+                    break;
+                case 'Consultations':
+                    this.consultationsVisible = true;
+                    break;
+                case 'Specialities':
+                    this.specialtiesVisible = true;
+                    break;
+                case 'Medications':
+                    this.medicationsVisible = true;
+                    break;
+                default:
+                    this.homeVisible = true;
+                    break;
+            }
+        },
+
+        setHome: function () {
+            this.setNav('');
+            this.pageTitle = 'Home';
+        },
+
+        getPractices: async function () {
+            this.setNav('Practices');
+            this.pageTitle = 'Practices';
+
+            isLoading = true;
+
+            const practicesUrl = `${this.baseUrl}/Practice`;
+            this.practices = await axios.get(practicesUrl)
+                .then(response => response.data.practices)
+                .catch(error => console.log(error))
+                .finally(() => { this.isLoading = false; });
+
+            console.log(this.practices);
+        },
+    },
+
+    filters: {
+        formatTime(value) {
+            return value.substring(11,16);
         }
+    }
 });
